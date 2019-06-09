521c4289bb9fc663e72d1346cbfa533e0dd8f6df
Richard Kraaijenhagen <stdin@riichard.com>
2019-06-09 22:14:57 +0200

    Added script to downsize images

---
+++ convertimages.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+function resize() {
+    file="$1"
+    out=$(basename "$file")
+    dir=$(basename $(dirname "$file"))
+    outpath=./images/ds/${dir}/${out}
+    mkdir -p "./images/ds/${dir}"
+    convert "${file}" -resize 1600x -quality 90 "${outpath}"
+}
+
+for file in $(find "./images/originals" -type f -name *.jpg); do
+    echo $file
+    resize "${file}"
+done
